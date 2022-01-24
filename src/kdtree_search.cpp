3fdd0a22dbf4e8525929e5d33c1d189c9c144f4d
piropann <tasogare1623@gmail.com>
2022-01-25 07:13:57 +0900

    update

---
+++ src/kdtree_search.cpp
@@ -0,0 +1,104 @@
+//kdtree
+//重心をもとに点群を減らす（ダウンサンプリング）
+#include <ros/ros.h>
+#include <ros/package.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/io/io.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    ros::init (argc, argv, "pub_pcl");
+    ros::NodeHandle nh;
+
+    srand (time (NULL));
+
+    //ROSでPCLを使う場合、パブリッシャーとサブスクライバーを使い、トピックを変換して処理する
+    //Pub/Subの設定
+    sensor_msgs::PointCloud2 cloud_ros;
+    ros::Publisher cloud_pub = nh.advertise<sensor_msgs::PointCloud2>("input",1);
+    ros::Publisher cloud_pub2 = nh.advertise<sensor_msgs::PointCloud2>("output",1);
+
+    //点群の格納場所を作成
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+
+    //点群ランダム生成
+    cloud->width = 1000;
+    cloud->height = 1;
+    cloud->points.resize (cloud->width * cloud->height);
+    for (std::size_t i = 0; i < cloud->size (); ++i)
+    {
+    (*cloud)[i].x = 1024.0f * rand () / (RAND_MAX + 1.0f);
+    (*cloud)[i].y = 1024.0f * rand () / (RAND_MAX + 1.0f);
+    (*cloud)[i].z = 1024.0f * rand () / (RAND_MAX + 1.0f);
+    }
+    
+    //kdtreeの実装
+    pcl::KdTreeFLANN<pcl::PointXYZ> kdtree;
+    kdtree.setInputCloud (cloud);//処理する点群
+    
+    pcl::PointXYZ searchPoint;
+
+    searchPoint.x = 1024.0f * rand () / (RAND_MAX + 1.0f);
+    searchPoint.y = 1024.0f * rand () / (RAND_MAX + 1.0f);
+    searchPoint.z = 1024.0f * rand () / (RAND_MAX + 1.0f);
+
+    // 選ばれた点に隣接するK個の点群を検索
+
+    int K = 10;
+
+    std::vector<int> pointIdxKNNSearch(K);
+    std::vector<float> pointKNNSquaredDistance(K);
+    
+    std::cout << "K nearest neighbor search at (" << searchPoint.x 
+                << " " << searchPoint.y 
+                << " " << searchPoint.z
+                << ") with K=" << K << std::endl;
+
+    if ( kdtree.nearestKSearch (searchPoint, K, pointIdxKNNSearch, pointKNNSquaredDistance) > 0 )
+    {
+        for (std::size_t i = 0; i < pointIdxKNNSearch.size (); ++i)
+            std::cout << "    "  <<   (*cloud)[ pointIdxKNNSearch[i] ].x 
+                    << " " << (*cloud)[ pointIdxKNNSearch[i] ].y 
+                    << " " << (*cloud)[ pointIdxKNNSearch[i] ].z 
+                    << " (squared distance: " << pointKNNSquaredDistance[i] << ")" << std::endl;
+    }
+
+    // Neighbors within radius search
+
+    std::vector<int> pointIdxRadiusSearch;
+    std::vector<float> pointRadiusSquaredDistance;
+
+    float radius = 256.0f * rand () / (RAND_MAX + 1.0f);
+
+    std::cout << "Neighbors within radius search at (" << searchPoint.x 
+                << " " << searchPoint.y 
+                << " " << searchPoint.z
+                << ") with radius=" << radius << std::endl;
+
+
+    if ( kdtree.radiusSearch (searchPoint, radius, pointIdxRadiusSearch, pointRadiusSquaredDistance) > 0 )
+    {
+        for (std::size_t i = 0; i < pointIdxRadiusSearch.size (); ++i)
+            std::cout << "    "  <<   (*cloud)[ pointIdxRadiusSearch[i] ].x 
+                        << " " << (*cloud)[ pointIdxRadiusSearch[i] ].y 
+                        << " " << (*cloud)[ pointIdxRadiusSearch[i] ].z 
+                        << " (squared distance: " << pointRadiusSquaredDistance[i] << ")" << std::endl;
+    }
+
+    //sensor_msgs::PointCloud2 cloud_ros;
+    pcl::toROSMsg(*cloud,cloud_ros);
+    cloud_ros.header.frame_id = "base_link";
+
+    ros::Rate loop_rate(1);
+    while(ros::ok()){
+        cloud_pub.publish(cloud_ros);
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+    return 0;
+}
---
+++ src/statistical_removal.cpp
@@ -0,0 +1,78 @@
+//StatisticalOutlierRemovalフィルター
+//外れ値や異常値を切り取る
+#include <ros/ros.h>
+#include <ros/package.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/io/io.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+#include <iostream>
+#include <pcl/common/transforms.h>
+
+pcl::PointCloud<pcl::PointXYZ>::Ptr loadPointCloud(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, std::string filename) {
+    //ファイルパスの取得
+    std::string file_path;
+    file_path = ros::package::getPath("ros-pcl-tutorials") + "/data/" + filename ;
+    pcl::io::loadPCDFile(file_path,*cloud);
+    return cloud;
+} 
+ 
+pcl::PointCloud<pcl::PointXYZRGB>::Ptr loadPointCloudRGB(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud, std::string filename) {
+    //ファイルパスの取得
+    std::string file_path;
+    file_path = ros::package::getPath("ros-pcl-tutorials") + "/data/" + filename ;
+    pcl::io::loadPCDFile(file_path,*cloud);
+    return cloud;
+} 
+
+
+int main(int argc, char **argv)
+{
+    ros::init (argc, argv, "pub_pcl");
+    ros::NodeHandle nh;
+
+    //ROSでPCLを使う場合、パブリッシャーとサブスクライバーを使い、トピックを変換して処理する
+    //Pub/Subの設定
+    sensor_msgs::PointCloud2 cloud_ros;
+    sensor_msgs::PointCloud2 cloud_ros2;
+    ros::Publisher cloud_pub = nh.advertise<sensor_msgs::PointCloud2>("input",1);
+    ros::Publisher cloud_pub2 = nh.advertise<sensor_msgs::PointCloud2>("output",1);
+
+    //点群の格納場所を作成
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
+
+    std::string file_path;
+    std::string file_name = "sample.pcd";
+
+    //点群読み込み
+    cloud = loadPointCloudRGB(cloud,file_name);
+
+    //処理をしたあとの点群の格納場所を作成
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZRGB>);
+
+    //Passthroughフィルターの実施
+    pcl::StatisticalOutlierRemoval<pcl::PointXYZRGB> sor;
+    sor.setInputCloud (cloud);//処理する点群
+    sor.setMeanK (50);//一つの点群が参照する点群数
+    sor.setStddevMulThresh (1.0);//平均距離の標準偏差値
+    sor.filter (*cloud_filtered);//処理を行ったあとの格納場所
+
+    //sensor_msgs::PointCloud2 cloud_ros;
+    pcl::toROSMsg(*cloud,cloud_ros);
+    pcl::toROSMsg(*cloud_filtered,cloud_ros2);
+    cloud_ros.header.frame_id = "base_link";
+    cloud_ros2.header.frame_id = "base_link";
+
+    pcl::io::savePCDFileBinary(ros::package::getPath("ros-pcl-tutorials") + "/data/result/statistical_" + file_name , *cloud_filtered);
+
+    ros::Rate loop_rate(1);
+    while(ros::ok()){
+        cloud_pub.publish(cloud_ros);
+        cloud_pub2.publish(cloud_ros2);
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+    return 0;
+}
